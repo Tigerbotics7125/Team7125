0ef363b2ed94dc6b501e5adac73544690f00a77b
Jeff Morris <ffejcsirrom@gmail.com>
2022-07-29 22:03:54 -0500

    test seperate header

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-const tabs = document.querySelectorAll("#top-tabs > div")
-
-
-
-for(let i = 0; i < tabs.length; i++) {
-    const tab = tabs[i]
-    tab.addEventListener('click', changeTab, false)
-}
-
-function changeTab() {
-    const selector = this.dataset.activate
-    if (selector) {
-        const el = document.querySelector(selector)
-        hideOtherContentAreas()
-        el.style.display = 'block'
-    }
-}
-
-function hideOtherContentAreas() {
-    const allContentAreas = document.querySelectorAll('#content-area > div')
-
-    for (let i = 0; i < allContentAreas.length; i++) {
-        allContentAreas[i].style.display = 'none'
-    }
-}
+// load the header and footer html files into their divs on page load.
+// I hate JS
+$(document).ready(function () {
+    $("#navbar").load("header.html")
+    $("#footer").load("footer.html")
+})
